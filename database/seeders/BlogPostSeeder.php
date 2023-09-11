dce741f7b7ceb6b22621e71204a49cf4da5670f2
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2023-09-11 20:41:07 +0600

    blog post data ready

---
+++ database/seeders/BlogPostSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BlogPost;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BlogPostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        BlogPost::factory(100)->create();
+    }
+}
